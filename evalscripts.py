7ae34029c55206262af929a30797685153de81ef
lschmidt <lschmidt@gasag.de>
2024-11-14 15:15:34 +0100

    Better NDVI evalscript

--- evalscripts.py
+++ evalscripts.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 
-class EvalScripts(Enum):
+class EvalScript(str, Enum):
+    # source: https://custom-scripts.sentinel-hub.com/sentinel-2/true_color/
     VISUAL = """
     //VERSION=3
 
@@ -17,57 +18,49 @@ class EvalScripts(Enum):
     }
 
     function evaluatePixel(sample) {
-        return [sample.B04, sample.B03, sample.B02];
+        return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
     }
     """
 
+    # source: https://custom-scripts.sentinel-hub.com/sentinel-2/ndvi/
     NDVI = """
     //VERSION=3
     function setup() {
     return {
-        input: [{
-        bands: [
-            "B04",
-            "B08",
-            "dataMask"
-        ]
-        }],
-        output: {
-        bands: 4
-        }
+        input: ["B04", "B08", "dataMask"],
+        output: { bands: 4 }
+    };
     }
-    }
-    
 
-    function evaluatePixel(sample) {
-        let val = (sample.B08 - sample.B04) / (sample.B08 + sample.B04);
-        let imgVals = null;
-        
-        if (val<-1.1) imgVals = [0,0,0];
-        else if (val<-0.2) imgVals = [0.75,0.75,0.75];
-        else if (val<-0.1) imgVals = [0.86,0.86,0.86];
-        else if (val<0) imgVals = [1,1,0.88];
-        else if (val<0.025) imgVals = [1,0.98,0.8];
-        else if (val<0.05) imgVals = [0.93,0.91,0.71];
-        else if (val<0.075) imgVals = [0.87,0.85,0.61];
-        else if (val<0.1) imgVals = [0.8,0.78,0.51];
-        else if (val<0.125) imgVals = [0.74,0.72,0.42];
-        else if (val<0.15) imgVals = [0.69,0.76,0.38];
-        else if (val<0.175) imgVals = [0.64,0.8,0.35];
-        else if (val<0.2) imgVals = [0.57,0.75,0.32];
-        else if (val<0.25) imgVals = [0.5,0.7,0.28];
-        else if (val<0.3) imgVals = [0.44,0.64,0.25];
-        else if (val<0.35) imgVals = [0.38,0.59,0.21];
-        else if (val<0.4) imgVals = [0.31,0.54,0.18];
-        else if (val<0.45) imgVals = [0.25,0.49,0.14];
-        else if (val<0.5) imgVals = [0.19,0.43,0.11];
-        else if (val<0.55) imgVals = [0.13,0.38,0.07];
-        else if (val<0.6) imgVals = [0.06,0.33,0.04];
-        else imgVals = [0,0.27,0];
-        
-        
-        imgVals.push(sample.dataMask)
-        
-        return imgVals
+    const ramp = [
+    [-0.5, 0x0c0c0c],
+    [-0.2, 0xbfbfbf],
+    [-0.1, 0xdbdbdb],
+    [0, 0xeaeaea],
+    [0.025, 0xfff9cc],
+    [0.05, 0xede8b5],
+    [0.075, 0xddd89b],
+    [0.1, 0xccc682],
+    [0.125, 0xbcb76b],
+    [0.15, 0xafc160],
+    [0.175, 0xa3cc59],
+    [0.2, 0x91bf51],
+    [0.25, 0x7fb247],
+    [0.3, 0x70a33f],
+    [0.35, 0x609635],
+    [0.4, 0x4f892d],
+    [0.45, 0x3f7c23],
+    [0.5, 0x306d1c],
+    [0.55, 0x216011],
+    [0.6, 0x0f540a],
+    [1, 0x004400],
+    ];
+
+    const visualizer = new ColorRampVisualizer(ramp);
+
+    function evaluatePixel(samples) {
+    let ndvi = index(samples.B08, samples.B04);
+    let imgVals = visualizer.process(ndvi);
+    return imgVals.concat(samples.dataMask)
     }
     """
